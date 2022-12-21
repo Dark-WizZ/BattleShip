9c32c0e950d0da773b22ee8bf85d6ddbdc70ce69
Dark-WizZ <Dark-WizZ>
2022-12-22 03:00:13 +0530

    Basic gameboard tests

--- src/modules/GameBoard.js
+++ src/modules/GameBoard.js
@@ -2,9 +2,15 @@ import Ship from '../modules/ship'
 
 export default class GameBoard{
   constructor(){
-    this.board = new Array[10][10];
+    this.board = new Array(10).fill().map(()=>new Array(10).fill());
+    this.ships = new Array();
   }
   place(x, y, len){
-
+    if(this.ships.length>4) return;
+    let s = new Ship(len);
+    this.ships.push(s);
+    for(let i=x; i<=x+len; i++){
+      this.board[x][y] = s;
+    }
   }
 }
--- src/test/GameBoard.test.js
+++ src/test/GameBoard.test.js
@@ -1,7 +1,19 @@
 import GameBoard from "../modules/GameBoard"
 
-test('wait',()=>{
+test('check placement at currect position',()=>{
   let gb = new GameBoard();
-  gb.place(2, 7, 3);
-  
+  let x=2, y=7, l=3;
+  gb.place(x, y, l);
+  let s1 = gb.ships[0];
+  for(let i=x; i<=x+l; i++){
+    expect(gb.board[x][y]).toBeDefined()
+    expect(gb.board[x][y]).toBe(s1);
+  }
+})
+
+test('check placement at wrong position',()=>{
+  let gb = new GameBoard();
+  let x=4, y=1, l=4;
+  gb.place(x, y, l);
+  expect(gb.board[1][2]).toBeUndefined()
 })
